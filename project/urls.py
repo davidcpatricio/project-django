caa1f3a2c6b52a6347f064aff31afa63a181ef2c
David Patrício <david.c.patricio@gmail.com>
2024-01-23 20:05:27 +0000

    Move views and URLs to app recipes

--- project/urls.py
+++ project/urls.py
@@ -15,25 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.http import HttpResponse
-from django.urls import path
-
-
-def my_view(request):
-    return HttpResponse('HOME')
-
-
-def about(request):
-    return HttpResponse('ABOUT')
-
-
-def contact(request):
-    return HttpResponse('CONTACT')
-
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', my_view),
-    path('about/', about),
-    path('contact/', contact),
+    path('', include('recipes.urls')),
 ]
