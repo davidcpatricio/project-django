865de814ed72bfcd3e68ec4458f0f93cd05e20d2
David Patrício <david.c.patricio@gmail.com>
2024-01-29 21:21:12 +0000

    Add recipe URL and view

--- recipes/views.py
+++ recipes/views.py
@@ -5,3 +5,9 @@ def home(request):
     return render(request, 'recipes/pages/home.html', context={
         'name': 'David Patrício'
     })
+
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/home.html', context={
+        'name': 'David Patrício'
+    })
