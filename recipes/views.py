bdcd9dff0b7987e065858d955f519bf92a6d0f7b
David Patrício <david.c.patricio@gmail.com>
2024-01-30 12:32:13 +0000

    Insert data into view home

--- recipes/views.py
+++ recipes/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
 
+from utils.recipes.factory import make_recipe
+
 
 def home(request):
     return render(request, 'recipes/pages/home.html', context={
-        'name': 'David Patrício'
+        'recipes': [make_recipe() for _ in range(10)],
     })
 
 
 def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'name': 'David Patrício'
+        'recipe': make_recipe(),
     })
