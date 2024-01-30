d406207c36280d8ea5577984a6903df793ff1c9f
David Patrício <david.c.patricio@gmail.com>
2024-01-30 16:48:35 +0000

    Registered category in Django admin

--- recipes/models.py
+++ recipes/models.py
@@ -5,6 +5,13 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=65)
 
+    class Meta:
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.name
+
 
 class Recipe(models.Model):
     title = models.CharField(max_length=65)
