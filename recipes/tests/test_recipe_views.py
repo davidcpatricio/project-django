dc8f26761ed205257ed60b67924b705a955cae25
David Patrício <david.c.patricio@gmail.com>
2024-02-15 18:56:09 +0000

    Created fixtures for a single test

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -1,7 +1,9 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
 from django.urls import resolve, reverse
 
 from recipes import views
+from recipes.models import Category, Recipe
 
 
 class RecipeViewsTest(TestCase):
@@ -23,6 +25,31 @@ class RecipeViewsTest(TestCase):
             '<h1>No recipes found here.</h1>',
             response.content.decode('utf-8')
         )
+
+    def test_recipe_home_template_loads_recipes(self):
+        category = Category.objects.create(name='Category')
+        author = User.objects.create_user(
+            first_name='user',
+            last_name='name',
+            username='username',
+            password='123456',
+            email='username@email.com',
+        )
+        recipe = Recipe.objects.create(
+            category=category,
+            author=author,
+            title='This is the recipe title',
+            description='This is the recipe description',
+            slug='recipe-slug',
+            preparation_time=10,
+            preparation_time_unit='Minutes',
+            servings=5,
+            servings_unit='Portions',
+            preparation_steps='Recipe Preparation Steps',
+            preparation_steps_is_html=False,
+            is_published=True,
+        )
+        assert 1 == 1
 
     def test_recipe_category_view_function_is_correct(self):
         view = resolve(reverse('recipes:category', kwargs={'category_id': 1}))
