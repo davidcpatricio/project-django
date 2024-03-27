5d984cc40d61a4aec5f470a75fe69ffbe568566b
David Patrício <david.c.patricio@gmail.com>
2024-03-27 15:13:25 +0000

    icontains filter and Q for complex querysets

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.http.response import Http404
 from django.shortcuts import get_list_or_404, get_object_or_404, render
 
@@ -42,7 +43,13 @@ def search(request):
     if not search_value:
         raise Http404()
 
+    recipes = Recipe.objects.filter(
+        Q(title__icontains=search_value) |
+        Q(description__icontains=search_value)
+    ).order_by('-id')
+
     return render(request, 'recipes/pages/search.html', {
         'page_title': f'Search | "{search_value}" | ',
         'search_value': search_value,
+        'recipes': recipes,
     })
